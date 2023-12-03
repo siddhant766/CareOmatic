c5115fbaad2ef2cfc9d85c740f3ab1ba3732c2a2
siddhant patel <152314055+siddhant766@users.noreply.github.com>
2023-12-03 17:55:55 +0530

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,15 @@
+#learning to create classes
+class Instructor:
+    def __init__(self,instructor_name,adress) :
+        self.name=instructor_name
+        self.adree=adress
+          
+        
+instructor_1=Instructor("Jesika","Siddhant")
+# instructor_1.name="sid"
+# instructor_1.roll_no="43"
+print(instructor_1.name)
+instructor_2=Instructor("cummins","solo")
+# instructor_2.name="sid"
+# instructor_2.roll_no="43"
+print(instructor_2.name)
